04458ceaf668bf705d0658819a027d30de4c5c01
Lucas Souza <lucassampsouza@gmail.com>
2025-05-16 21:57:41 -0300

    Customizado Login

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,7 +3,8 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
+import "./main.css";
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
